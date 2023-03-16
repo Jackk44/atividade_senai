ba0542f9415144633fd4635e9fd49e3b710a2934
jackson galindo <jackson.galindo2018@gmail.com>
2023-03-16 10:05:33 -0300

    atividade

---
+++ Pessoa_Fisica.cs
@@ -0,0 +1,9 @@
+namespace Atividade
+{
+    class Pessoa_Fisica : Clientes
+   {
+    public string cpf {get; set;}
+    public string data_de_nascimento {get; set;}
+    public string rg {get; set;}
+   } 
+}
